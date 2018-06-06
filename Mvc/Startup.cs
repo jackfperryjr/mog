50843510728c21d3c74d0b08ee5157fb422bf0db
jack f perry jr <jackperryjr@cid.local>
2018-06-06 18:07:23 -0400

    Changed comment in startup

--- Mvc/Startup.cs
+++ Mvc/Startup.cs
@@ -26,11 +26,11 @@ namespace Mvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Adding database for users.
+            // Adding database for Users.
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(Configuration.GetConnectionString("UserConnection")));
 
-            // Adding database for characters.
+            // Adding database for Final Fantasy.
             services.AddDbContext<CharacterContext>(options =>
                 options.UseSqlite(Configuration.GetConnectionString("CharacterConnection")));
 
