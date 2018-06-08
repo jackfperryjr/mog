6804548c1abb8bdff053d3cf403219f346cb3424
jack f perry jr <jackperryjr@cid.local>
2018-06-08 07:31:14 -0400

    Removed inline css to fix mobile responsiveness and added Aranea

--- Mvc/wwwroot/js/site.js
+++ Mvc/wwwroot/js/site.js
@@ -116,11 +116,11 @@ $('#search').keyup(function() {
         // Display character format
         $.each(characters, function(key, character) {
             if ((character.name.search(regex) != -1)) {
-                output += '<div class="row" style="margin-right: 0;margin-left:0;"><div class="col-md-8">';
-                output += '<h4 style="color: #2962ff;margin-bottom: -.5px;"><strong>'+ character.name + '</strong></h4>';
+                output += '<div class="row moogle-row"><div class="col-md-8">';
+                output += '<h4 class="moogle-h4"><strong>'+ character.name + '</strong></h4>';
                 output += '<h7 class="search-result">To learn more visit <a href="https://www.google.com/search?q=final+fantasy+' + character.name.toLowerCase() + '" target="_blank">https://www.google.com/search?q=' + character.name.toLowerCase() + '</a></h7><br/>';
-                output += '<p style="color: #37474f">' + character.description + '</p>';
-                output += '<table style="color: #37474f; width:50%;"><tr>';
+                output += '<p class="dark-bluish-gray">' + character.description + '</p>';
+                output += '<table class="moogle-table"><tr>';
                 output += '<td><strong>Age:</strong> ' + character.age + '</td>';
                 output += '<td><strong>Race:</strong> ' + character.race + '</td></tr><tr>';
                 output += '<td><strong>Job:</strong> ' + character.job + '</td>';
@@ -128,8 +128,9 @@ $('#search').keyup(function() {
                 output += '<td><strong>Weight:</strong> ' + character.weight + 'kg</td>';
                 output += '<td><strong>Origin:</strong> ' + character.origin + '</td></tr>';
                 output += '</table><br/></div>';
-                output += '<button type="button" style="background:transparent; padding: 10px; border: 1px solid #e0e0e0; margin: 20px 0; float: right;" data-toggle="modal" data-target="#imageModal">';
-                output += '<img style="width: 200px;" src=' + character.picture + ' alt="Picture of ' + character.name  + '." title="Picture of ' + character.name  + '."></button>';
+                output += '<div class="col-md-4">';
+                output += '<button type="button" class="moogle-modal-button" data-toggle="modal" data-target="#imageModal">';
+                output += '<img class="moogle-img" src=' + character.picture + ' alt="Picture of ' + character.name  + '." title="Picture of ' + character.name  + '."></button>';
                 output += '<div id="imageModal" class="modal fade" role="dialog">';
                 output += '<div class="modal-dialog">';
                 output += '<div class="modal-content">';
@@ -137,7 +138,7 @@ $('#search').keyup(function() {
                 output += '<button type="button" class="close" data-dismiss="modal">X</button>';
                 output += '</div>';
                 output += '<div class="modal-body">';
-                output += '<img style="width: 100%;" src=' + character.picture + ' alt="Picture of ' + character.name  + '." title="Picture of ' + character.name  + '."></div>';
+                output += '<img class="moogle-modal-img" src=' + character.picture + ' alt="Picture of ' + character.name  + '." title="Picture of ' + character.name  + '."></div>';
                 output += '</div></div></div>';
                 output += '<br/></div></div>';
                 if(count%2 == 0) { 
@@ -148,16 +149,16 @@ $('#search').keyup(function() {
         });
         $.each(monsters, function(key, monster) {
             if ((monster.name.search(regex) != -1)) {
-                output += '<div class="row" style="margin-right: 0;margin-left:0;"><div class="col-md-8">';
-                output += '<h4 style="color: #2962ff;margin-bottom: -.5px;"><strong>'+ monster.name + '</strong></h4>';
+                output += '<div class="row moogle-row"><div class="col-md-8">';
+                output += '<h4 class="moogle-h4"><strong>'+ monster.name + '</strong></h4>';
                 output += '<h7 class="search-result">To learn more visit <a href="https://www.google.com/search?q=final+fantasy+' + monster.name.toLowerCase() + '" target="_blank">https://www.google.com/search?q=' + monster.name.toLowerCase() + '</a></h7><br/>';
-                output += '<p style="color: #37474f">' + monster.description + '</p>';
-                output += '<table style="color: #37474f; width:50%;"><tr>';
+                output += '<p class="dark-bluish-gray">' + monster.description + '</p>';
+                output += '<table class="moogle-table"><tr>';
                 output += '<td><strong>Strength:</strong> ' + monster.strength + '</td>';
-                output += '<td><strong>Weakness:</strong> ' + monster.weakness + '</td></tr></table>';
+                output += '<td><strong>Weakness:</strong> ' + monster.weakness + '</td></tr></table><br/>';
                 output += '</div><div class="col-md-4">';
-                output += '<button type="button" style="background:transparent; padding: 10px; border: 1px solid #e0e0e0; margin: 20px 0; float: right;" data-toggle="modal" data-target="#imageModal">';
-                output += '<img style="width: 200px;" src=' + monster.picture + ' alt="Picture of ' + monster.name  + '." title="Picture of ' + monster.name  + '."></button>';
+                output += '<button type="button" class="moogle-modal-button" data-toggle="modal" data-target="#imageModal">';
+                output += '<img class="moogle-img" src=' + monster.picture + ' alt="Picture of ' + monster.name  + '." title="Picture of ' + monster.name  + '."></button>';
                 output += '<div id="imageModal" class="modal fade" role="dialog">';
                 output += '<div class="modal-dialog">';
                 output += '<div class="modal-content">';
@@ -165,7 +166,7 @@ $('#search').keyup(function() {
                 output += '<button type="button" class="close" data-dismiss="modal">X</button>';
                 output += '</div>';
                 output += '<div class="modal-body">';
-                output += '<img style="width: 100%;" src=' + monster.picture + ' alt="Picture of ' + monster.name  + '." title="Picture of ' + monster.name  + '."></div>';
+                output += '<img class="moogle-modal-img" src=' + monster.picture + ' alt="Picture of ' + monster.name  + '." title="Picture of ' + monster.name  + '."></div>';
                 output += '</div></div></div>';
                 output += '<br/></div></div>';
                 if(count%2 == 0) { 
@@ -176,13 +177,13 @@ $('#search').keyup(function() {
         });
         $.each(games, function(key, game) {
             if ((game.title.search(regex) != -1)) {
-                output += '<div class="row" style="margin-right: 0;margin-left:0;"><div class="col-md-8">';
-                output += '<h4 style="color: #2962ff;margin-bottom: -.5px;"><strong>'+ game.title + '</strong></h4>';
+                output += '<div class="row moogle-row"><div class="col-md-8">';
+                output += '<h4 class="moogle-h4"><strong>'+ game.title + '</strong></h4>';
                 output += '<h7 class="search-result">To learn more visit <a href="https://www.google.com/search?q=final+fantasy+' + game.title.toLowerCase() + '" target="_blank">https://www.google.com/search?q=' + game.title.toLowerCase() + '</a></h7><br/>';
-                //output += '<p style="color: #37474f">' + game.description + '</p>';
-                output += '</div><div class="col-md-4">';
-                output += '<img style="width: 200px; padding: 10px; border: 1px solid #e0e0e0; margin: 20px 0; float: right;" src=' + game.picture + ' alt="Logo for ' + game.title + '." title="Picture of ' + game.title + '.">';
-                output += '<br/></div></div>';
+                //output += '<p class="dark-bluish-gray">' + game.description + '</p>';
+                output += '</div><br/><div class="col-md-4">';
+                output += '<img class="moogle-img" src=' + game.picture + ' alt="Logo for ' + game.title + '." title="Picture of ' + game.title + '.">';
+                output += '<br/></div><br/></div>';
                 if(count%2 == 0) { 
                     output += '</div>'
                 }
