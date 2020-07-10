12123c7b3d0efe4d2b0c83728b776734123c0cce
jackfperryjr <jack.franklin.perryjr@gmail.com>
2020-07-10 13:20:11 -0400

    Fixed a few things with Vue, added Bootstrap4 css and js, updated the README, I think. WIP.

--- Mog.Api/wwwroot/js/site.js
+++ Mog.Api/wwwroot/js/site.js
@@ -1,5 +1,3 @@
-let url = window.location.origin;
-
 // List for setting a random search example name as the input placeholder
 let randomPlaceholderList = [
     "Search ex. ''Lightning'' or ''13''",
@@ -30,7 +28,7 @@ new Vue({
     },
     methods: {
         getCharacters() {
-            axios.get(url + "/api/v1/characters")
+            axios.get("https://www.moogleapi.com/api/v1/characters")
             .then(response => {this.character = response.data})
         },
         setModal(character) {
@@ -42,15 +40,15 @@ new Vue({
         getRandom() {
             $(".search-results").hide()
             $("#random-character").show()
-            axios.get(url + "/api/v1/characters/random")
+            axios.get("https://www.moogleapi.com/api/v1/characters/random")
             .then(response => {this.random = response.data})
         },
         getLoad() {
             $(".search-results").hide()
             $("#random-character").show()
 
-            axios.get(url + "/api/v1/characters/75c054fe-d022-44d4-102f-08d6afcab3e2") // Using id (returns single entity)
-            // axios.get(url + "/api/v1/characters/search?name=lightning") // Using search route (returns array so rendering would take a different approach)
+            //axios.get(url + "/api/v1/characters/75c054fe-d022-44d4-102f-08d6afcab3e2") // Using id (returns single entity)
+            axios.get("https://www.moogleapi.com/api/v1/characters/search?name=lightning") // Using search route (returns array so rendering would take a different approach)
             .then(response => {this.random = response.data})
         }
     },
@@ -68,7 +66,7 @@ new Vue({
                 .filter(function(character) {
                     return character.name.toLowerCase().indexOf(self.search) > -1
                     || character.name.indexOf(self.search) > -1
-                    || character.origin.indexOf(self.search) > -1; 
+                    || character.origin.toLowerCase().indexOf(self.search) > -1; 
                 });    
                 
                 //filtered = this.character
