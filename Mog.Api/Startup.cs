a2134375cd23f28bee75cceabb7459eb371c34b0
jackfperryjr <jack.franklin.perryjr@gmail.com>
2020-07-16 18:35:31 -0400

    Moved SwaggerDocs out of the else block.

--- Mog.Api/Startup.cs
+++ Mog.Api/Startup.cs
@@ -103,11 +103,8 @@ namespace Mog.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            else 
-            {
-                app.UseSwaggerDocuments();
-            }
             
+            app.UseSwaggerDocuments();
             app.UseRouting();
             app.UseCors(x => x
                 .AllowAnyOrigin()
