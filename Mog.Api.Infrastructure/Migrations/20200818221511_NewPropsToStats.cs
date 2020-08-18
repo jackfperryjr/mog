5c027ccd70bbd1f54c8ddd0639a7883f7ef45bd9
jackfperryjr <jack.franklin.perryjr@gmail.com>
2020-08-18 18:37:38 -0400

    Added new properties to the Stat model.

---
+++ Mog.Api.Infrastructure/Migrations/20200818221511_NewPropsToStats.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mog.Api.Infrastructure.Migrations
+{
+    public partial class NewPropsToStats : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Class",
+                table: "Stats",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Level",
+                table: "Stats",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Class",
+                table: "Stats");
+
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "Stats");
+        }
+    }
+}
