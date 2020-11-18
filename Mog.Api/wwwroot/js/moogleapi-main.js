29d36521e95b64059aa86452f1f52683c79775ea
jackfperryjr <jackfperryjr@gmail.com>
2020-11-18 14:46:41 -0500

    Added count to the homagepage again.

--- Mog.Api/wwwroot/js/moogleapi-main.js
+++ Mog.Api/wwwroot/js/moogleapi-main.js
@@ -2,4 +2,16 @@ $(document).ready(function() {
     if (halfmoon.readCookie("darkModeOn") == "yes") {
         halfmoon.toggleDarkMode();
     } 
+
+    getCount();
 });
+
+function getCount() {
+    fetch("https://www.moogleapi.com/api/v1/characters")
+        .then(function(response) {
+            return response.json();
+      }).then(function(data) {
+            console.log(data.length);
+            $("#character-count").text(data.length);
+      });
+}
